969c068c02ce85fbf6ebc2bf864fb494692e9c9a
Nick Popov <marketing.popov@gmail.com>
2023-01-22 17:46:18 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View style={{ width: 100, height: 100, backgroundColor: 'red' }} />
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
